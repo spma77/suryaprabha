5290ffa817fdc0fd1873cb67cb4ff82033caaa72
suryaprabha <suryaprabha.v@gmail.com>
2016-09-21 17:37:44 -0700

    Talent Works Login Automation

---
+++ TalentWorksLogin/src/com/testing/suryaprabha/TalentWorksAutomation.java
@@ -0,0 +1,50 @@
+package com.testing.suryaprabha;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TalentWorksAutomation {
+	public static void GoogleLogin() {
+		WebDriver driver = new FirefoxDriver();
+
+		driver.get("https://dev.talentworks.io/#/login");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.MICROSECONDS);
+
+		WebElement element = driver.findElement(By.xpath("html/body/data/div/div/div/div[3]/a[1]"));
+		element.click();
+		
+		//Get handle to the login window
+		for (String winHandle : driver.getWindowHandles()) {
+			driver.switchTo().window(winHandle);
+			driver.manage().window().maximize();
+		}
+
+		WebElement userName = driver.findElement(By.xpath(".//*[@id='Email']"));
+		userName.sendKeys("test@gmail.com");
+		driver.findElement(By.id("next")).click();
+		
+		//This is to get handle to the password window
+		for (String winHandle : driver.getWindowHandles()) {
+			driver.switchTo().window(winHandle);
+			driver.manage().window().maximize();
+		}
+
+		WebElement passWord = driver.findElement(By.xpath(".//*[@id='Passwd']"));
+		passWord.sendKeys("lskjgksjg");
+
+		WebElement signIn = driver.findElement(By.xpath(".//*[@id='signIn']"));
+		signIn.click();
+		
+	}
+	
+	public static void main(String[] args) {
+		TalentWorksAutomation.GoogleLogin();
+	}
+
+}
+
